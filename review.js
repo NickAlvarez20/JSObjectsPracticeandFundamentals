72a7875831d3ded21e78fa50295823579b8b2d25
NickAlvarez20 <NicholasAAlvarez20@gmail.com>
2025-02-18 20:40:49 -0700

    Completed exercise 3, which used object.create() and then assigned new property, tested the original method(use), then overrode the method, tested the override, and completed the exercises for day 1 learning of objects

--- review.js
+++ review.js
@@ -24,4 +24,23 @@ const Loonch = new Meal("Tuna", "Lunch", 3);
 Brecky.describe();
 Loonch.describe();
 
+
+const baseTool = {
+    use() {
+        console.log(`Using ${this.name}`);
+    }
+}
+
+const knife = Object.create(baseTool);
+knife.name = "Knife";
+knife.use();
+
+//Override use tool
+knife.use = function () {
+    console.log(`You have devolved into caveman, you do not know what a ${this.name} is.`)
+}
+knife.use();
+
+
+
 // node review.js
